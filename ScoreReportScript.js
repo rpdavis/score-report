3395459e681d1acabd7cc34ba9b8a3e169d71d1e
rpdavis <bcrash01@yahoo.com>
2024-03-04 13:25:13 -0800

    Update ScoreReportScript.js

--- ScoreReportScript.js
+++ ScoreReportScript.js
@@ -261,11 +261,10 @@
             newHTML += `</p>`;
         }
     });
-   newHTML +=  `<textarea id="IntroBodyContent">
-    <p><strong>I. Reason for Referral:</strong></p>
-    <p>${firstName} was referred for this academic assessment as part of a 3-year re-evaluation for special education services. (Please see the psycho-educational report for additional information).</p>
-    
-</textarea>`;
+newHTML += `<textarea id="IntroBodyContent">`;
+newHTML += `<p><strong>I. Reason for Referral:</strong></p>`;
+newHTML += `<p>${firstName} was referred for this academic assessment as part of a 3-year re-evaluation for special education services. (Please see the psycho-educational report for additional information).</p>`;
+newHTML += `</textarea>`;
 
     // Close the HTML content
     newHTML += `
