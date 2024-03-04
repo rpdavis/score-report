39168ba2938f99f9eeb5f38e622fb4c7b6f2de01
rpdavis <bcrash01@yahoo.com>
2024-03-04 12:28:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
  import('./ScoreReportScript.js')
   .catch(error => console.error('Error importing bodyText.js:', error));
+ import('./bodyText.js')
+  .catch(error => console.error('Error importing bodyText.js:', error));
