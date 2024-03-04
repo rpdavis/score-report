98be737d11253c12dd33f319dcc7267876233864
rpdavis <bcrash01@yahoo.com>
2024-03-04 12:44:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
   .catch(error => console.error('Error importing bodyText.js:', error));
  import('./bodyText.js')
   .catch(error => console.error('Error importing bodyText.js:', error));
+
+document.addEventListener("DOMContentLoaded", function() {
+    var introBodyTextarea = document.getElementById("IntroBodyContent");
+    if (introBodyTextarea) {
+        introBodyTextarea.value = IntroBodyContent;
+    } else {
+        console.error("Textarea element with id 'IntroBodyContent' not found.");
+    }
+});
